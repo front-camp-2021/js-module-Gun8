a7cc7c085c8318bf0b41046ff2ecb7b31b5b55c2
Vladislav Polovinkin <joy2304k@gmail.com>
2021-10-04 00:02:44 +0300

    filters-list done with event listeners

--- src/module-4/filters-list/index.js
+++ src/module-4/filters-list/index.js
@@ -1,11 +1,115 @@
 export default class FiltersList {
+  element;
+  subElements;
   constructor ({
      title = '',
      list = []
   } = {}) {
-    this.title = title;
-    this.list = list;
+    this.state = {
+      title: title,
+      list: list
+    };
 
-    // ... your logic
+    this.render();
+    this.getSubElements();
+    this.update(this.state);
   }
+
+  getTemplate(){
+    return `
+        <div class="filters__item">
+            <h3 class="filters__list-name" data-element="title">${this.state.title}</h3>
+            <ul class="filters__list" data-element="list"></ul>
+        </div>
+    `;
+  }
+
+  update({title = '', list = []} = {}){
+    this.state = {title,list};
+
+    this.subElements.list.innerHTML = "";
+    this.subElements.title.innerHTML = title;
+
+     list.map(item => {
+      const wrapper = document.createElement('div');
+
+      wrapper.innerHTML = this.getFiltersListItem(item);
+
+      if(item.checked){
+        const input = wrapper.querySelector("input");
+        input.checked = true;
+        this.check(input);
+      }
+
+      this.subElements.list.append(wrapper.firstElementChild);
+    })
+  }
+
+
+
+  getFiltersListItem({value,title}){
+    return `
+    <li class="filters__list-item">
+            <div>
+              <input type="checkbox" value="${value}" name="${this.state.title}" id="${value}">
+              <label for="${value}">
+                <span class="filters__checkbox"></span>
+                <span class="filters__val">${title}</span>
+              </label>
+            </div>
+     </li>
+    `
+  }
+
+  check(target){
+    if(target.checked){
+      target.nextElementSibling.firstElementChild.classList.add("filters__checkbox_checked");
+      return;
+    }
+
+    target.nextElementSibling.firstElementChild.classList.remove("filters__checkbox_checked");
+  }
+
+  reset(){
+    const inputs = this.element.querySelectorAll("input");
+
+    for(let i = 0; i < inputs.length; i++){
+      inputs.item(i).checked = false;
+      this.check(inputs.item(i));
+    }
+  }
+
+  render(){
+    const wrapper = document.createElement('div');
+
+    wrapper.innerHTML  = this.getTemplate();
+
+    this.element = wrapper.firstElementChild;
+  }
+
+  getSubElements(){
+    const result = {};
+    const elements = this.element.querySelectorAll('[data-element]');
+
+    for(const subElement of elements){
+      const name = subElement.dataset.element;
+
+      result[name] = subElement;
+    }
+
+    this.subElements = result;
+  }
+
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+  }
+
 }
