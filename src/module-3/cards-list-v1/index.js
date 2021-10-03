c480a3be706e52c212c0cf70b1ae59bab2a63c33
Vladislav Polovinkin <joy2304k@gmail.com>
2021-10-03 05:13:16 +0300

    module-3 done

--- src/module-3/cards-list-v1/index.js
+++ src/module-3/cards-list-v1/index.js
@@ -3,6 +3,41 @@ export default class CardsList {
     this.data = data;
     this.Component = Component;
 
-    // ... your logic
+    this.render(this.data);
+  }
+
+  getTemplate (){
+    return `<div class="item-list"></div>`
+  }
+
+  render(){
+    const wrapper = document.createElement('div');
+
+    wrapper.innerHTML  = this.getTemplate();
+
+    const CardsList = wrapper.querySelector('.item-list');
+
+    this.data.forEach(obj =>  CardsList.appendChild((new this.Component(obj)).element));
+
+    this.element = CardsList;
+  }
+
+  update(data = []){
+    this.data = data;
+    this.remove();
+    this.render();
+  }
+
+  remove(){
+    if(this.element){
+      this.element.remove();
+    }
+  }
+
+  destroy(){
+    this.remove();
+    this.element = null;
   }
 }
+
+
